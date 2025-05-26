17586c70e90a99d1f27b2f650e64d81ae20783e8
mwunnam <mwunnam@gmail.com>
2025-05-26 16:47:32 +0000

    done with all requirements

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,56 +1,3 @@
-interface Teacher {
-  firstName: string;
-  lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  contract?: boolean;
-  [key: string]: any;
-}
-
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName[0]}. ${lastName}`;
-};
-
-
-// Interface for the class constructor
-interface StudentConstructor {
-  new (firtName: string, lastName: string): StudentClassInterface;
-}
-
-// Interface describing the methods of the student class
-interface StudentClassInterface {
-  workOnHomework(): string;
-  displayName(): string;
-}
-
-// Class implementing the student interface
-class StudentClass implements StudentClassInterface {
-  private firstName: string;
-  private lastName: string;
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework(): string {
-    return `Currently working`;
-  }
-
-  displayName(): string {
-    return this.firstName;
-  }
-}
-
 // Interface for Director
 interface DirectorInterface {
   workFromHome(): string;
@@ -62,7 +9,7 @@ interface DirectorInterface {
 interface TeacherInterface {
   workdFromHome(): string;
   getCoffeeBreak(): string;
-  workTeacherTask(): string;
+  workTeacherTasks(): string;
 }
 
 
@@ -71,7 +18,10 @@ class Director implements DirectorInterface {
   workFromHome(): string {
     return `Working from home`;
   };
-
+  
+  getCoffeeBreak(): string {
+    return  'Break time';
+  }
   getToWork(): string {
     return `Getting a coffee break`;
   };
@@ -82,10 +32,10 @@ class Director implements DirectorInterface {
 };
 
 class Teacher implements TeacherInterface {
-  workFromHome(): string {
+  workdFromHome(): string {
     return `Cannot work from home`;
   }
-
+  
   getCoffeeBreak(): string {
     return `Cannot have a break`;
   }
@@ -117,7 +67,6 @@ function executeWork(employee: Teacher | Director){
 }
 
 
-// Task 7
 // Define a string Literal type
 type Subjects = 'Math' | 'History';
 
