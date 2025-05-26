badf1784c72773b08b068931640905c1d1d428e0
mwunnam <mwunnam@gmail.com>
2025-05-26 16:20:31 +0000

    done with task 11

---
+++ 0x04-TypeScript/task_5/js/subjects/Java.ts
@@ -0,0 +1,22 @@
+/// <reference path="./Subject.ts" />
+/// <reference path="./Teacher.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;   
+  }
+
+  export class Java extends Subjects.Subject {
+    getRequirements(): string {
+        return ' Here is the list of requirements for React'
+    };
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || this.teacher.experienceTeachingC === undefined || this.teacher.experienceTeachingC <= 0) {
+        return 'No available teacher';
+      }   
+      return `Available Teacher: ${this.teacher.firstName}`; 
+    }
+
+  }
+}
